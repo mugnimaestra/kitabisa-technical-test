8a51c5b83570f1cf8c265ac6769d3617992ae566
mugnimaestra <mugnimaestra2@gmail.com>
2020-04-30 11:39:42 +0700

    Updates

---
+++ precache-manifest.66a08682ccafe8198a907dad4661f482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffee63edc5d6bd619d66b49ea1b7f921",
+    "url": "/kitabisa-technical-test/index.html"
+  },
+  {
+    "revision": "723af05df7b209f6d8be",
+    "url": "/kitabisa-technical-test/static/css/main.3c371430.chunk.css"
+  },
+  {
+    "revision": "4b977c93f91da536fc49",
+    "url": "/kitabisa-technical-test/static/js/2.b01fd924.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kitabisa-technical-test/static/js/2.b01fd924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723af05df7b209f6d8be",
+    "url": "/kitabisa-technical-test/static/js/main.c53f3cc2.chunk.js"
+  },
+  {
+    "revision": "4ede5df6be2abd493a1b",
+    "url": "/kitabisa-technical-test/static/js/runtime-main.6e8b3a0e.js"
+  }
+]);
